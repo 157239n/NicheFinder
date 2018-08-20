7297e061f0a69b6870e17a0c43e420b99fdeeb88
157239n <157239q@gmail.com>
2018-08-20 09:49:38 +0700

    Initial commit

---
+++ src/test/java/com/n157239/niche_finder/MainTest.java
@@ -0,0 +1,13 @@
+package com.n157239.niche_finder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void someInitialTest(){
+        assert true;
+        assertTrue(true);
+    }
+}
